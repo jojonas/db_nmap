20c4cf1258fde68a807e912a79e2144dc8f4d342
Jonas Lieb <lieb.jonas@gmail.com>
2021-10-12 23:50:18 +0200

    Add full timestamp

--- internal/logging.go
+++ internal/logging.go
@@ -9,4 +9,11 @@ var log = Logger
 
 func init() {
 	log.SetLevel(logrus.DebugLevel)
+
+	formatter := &logrus.TextFormatter{
+		TimestampFormat: "2006-01-02 15:04:05",
+		FullTimestamp:   true,
+	}
+
+	log.SetFormatter(formatter)
 }
