7a7abd0821421082c67ec5a6ed4734799a8c4135
Jonas Lieb <lieb.jonas@gmail.com>
2022-01-26 20:26:14 +0100

    Version without ORM

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/jojonas/db_nmap
 go 1.17
 
 require (
+	github.com/georgysavva/scany v0.3.0
 	github.com/jackc/pgx/v4 v4.13.0
 	github.com/sirupsen/logrus v1.4.2
 )
@@ -15,8 +16,11 @@ require (
 	github.com/jackc/pgproto3/v2 v2.1.1 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20200714003250-2b9c44734f2b // indirect
 	github.com/jackc/pgtype v1.8.1 // indirect
+	github.com/jackc/puddle v1.1.3 // indirect
 	github.com/konsorten/go-windows-terminal-sequences v1.0.2 // indirect
+	github.com/lib/pq v1.10.4 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
 	golang.org/x/crypto v0.0.0-20210711020723-a769d52b0f97 // indirect
-	golang.org/x/sys v0.0.0-20210615035016-665e8c7367d1 // indirect
+	golang.org/x/sys v0.0.0-20210823070655-63515b42dcdf // indirect
 	golang.org/x/text v0.3.6 // indirect
 )
--- internal/msf_db.go
+++ internal/msf_db.go
@@ -2,11 +2,40 @@ package internal
 
 import (
 	"context"
+	"errors"
 	"fmt"
+	"net"
 	"time"
 
 	"github.com/jackc/pgx/v4"
 )
+
+type MsfHost struct {
+	Id          int
+	WorkspaceId int
+	Address     net.IP
+
+	MAC     string
+	Name    string
+	State   string
+	OSName  string
+	Purpose string
+
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
+
+type MsfService struct {
+	Id        int
+	HostId    int
+	CreatedAt time.Time
+	Port      int
+	Proto     string
+	State     string
+	Name      string
+	UpdatedAt time.Time
+	Info      string
+}
 
 func GetWorkspaceId(ctx context.Context, conn *pgx.Conn, workspaceName string) (int, error) {
 	row := conn.QueryRow(ctx, "SELECT id FROM workspaces WHERE name=$1 LIMIT 1", workspaceName)
@@ -20,87 +49,162 @@ func GetWorkspaceId(ctx context.Context, conn *pgx.Conn, workspaceName string) (
 	return workspaceId, nil
 }
 
-func InsertHost(ctx context.Context, conn *pgx.Conn, workspaceId int, host NmapHost) (int, error) {
-	if !host.HasOpenPorts() {
-		log.Debugf("Host %s does not have any open ports, skipping.", host)
+func InsertHost(ctx context.Context, conn *pgx.Conn, workspaceId int, nmapHost NmapHost) (int, error) {
+	if !nmapHost.HasOpenPorts() {
+		log.Debugf("Host %s does not have any open ports, skipping.", nmapHost)
 		return 0, nil
+	}
+
+	now := time.Now()
+
+	allIPs := nmapHost.AllIPAddresses()
+	var preferredIP net.IP
+	if len(allIPs) > 0 {
+		preferredIP = net.ParseIP(allIPs[0].String())
 	}
 
 	tx, err := conn.Begin(ctx)
 	if err != nil {
-		return 0, fmt.Errorf("beginning transaction: %w", err)
+		return 0, fmt.Errorf("begin transaction: %w", err)
 	}
 	defer tx.Rollback(ctx)
 
-	now := time.Now()
+	msfHost := MsfHost{}
 
-	osName := "Unknown"
-	if len(host.Os.Osmatch) > 0 {
-		osName = host.Os.Osmatch[0].Name
+	err = tx.QueryRow(ctx,
+		`SELECT 
+				id, 
+				workspace_id, 
+				address, 
+				mac, 
+				name, 
+				state, 
+				os_name, 
+				purpose,
+				created_at, 
+				updated_at
+			FROM hosts 
+			WHERE workspace_id=$1 AND address=$2
+			LIMIT 1
+			FOR UPDATE`,
+		workspaceId,
+		preferredIP.String(),
+	).Scan(
+		&msfHost.Id,
+		&msfHost.WorkspaceId,
+		&msfHost.Address,
+		&msfHost.MAC,
+		&msfHost.Name,
+		&msfHost.State,
+		&msfHost.OSName,
+		&msfHost.Purpose,
+		&msfHost.CreatedAt,
+		&msfHost.UpdatedAt,
+	)
+
+	if errors.Is(err, pgx.ErrNoRows) {
+		log.Debugf("host %v not found in database", preferredIP)
+	} else if err != nil {
+		return 0, fmt.Errorf("query host %v: %w", preferredIP, err)
 	}
 
-	purpose := "device"
-	if len(host.Os.Osclass) > 0 {
-		purpose = host.Os.Osclass[0].Type
-	}
+	msfHost.WorkspaceId = workspaceId
+	msfHost.Address = preferredIP
 
-	allMacs := host.AllMacAddresses()
-	preferredMac := ""
+	allMacs := nmapHost.AllMacAddresses()
 	if len(allMacs) > 0 {
-		preferredMac = allMacs[0].String()
+		msfHost.MAC = allMacs[0].String()
 	}
 
-	allIPs := host.AllIPAddresses()
-	preferredIP := ""
-	if len(allIPs) > 0 {
-		preferredIP = allIPs[0].String()
-	}
-
-	allHostnames := host.AllHostnames()
-	preferredHostname := ""
+	allHostnames := nmapHost.AllHostnames()
 	if len(allHostnames) > 0 {
-		preferredHostname = allHostnames[0]
+		msfHost.Name = allHostnames[0]
 	}
 
-	var hostId int
+	msfHost.State = "alive"
+
+	if len(nmapHost.Os.Osmatch) > 0 {
+		msfHost.OSName = nmapHost.Os.Osmatch[0].Name
+	}
+
+	if msfHost.Purpose == "" {
+		msfHost.Purpose = "device"
+	}
+
+	if len(nmapHost.Os.Osclass) > 0 {
+		msfHost.Purpose = nmapHost.Os.Osclass[0].Type
+	}
+
+	if msfHost.CreatedAt.IsZero() {
+		msfHost.CreatedAt = now
+	}
+
+	msfHost.UpdatedAt = now
+
 	err = tx.QueryRow(ctx,
 		`INSERT INTO hosts (
-			created_at, 
-			mac,
+			workspace_id, 
 			address, 
+			mac, 
 			name, 
 			state, 
 			os_name, 
-			workspace_id, 
-			updated_at, 
-			purpose 
+			purpose,
+			created_at, 
+			updated_at
 		) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)
-		ON CONFLICT (address, workspace_id) DO UPDATE 
-		SET updated_at = $1, mac = $2, name = $4, state = $5, os_name = $6, purpose = $9
-		RETURNING id`,
-
-		now,               // created_at
-		preferredMac,      // mac
-		preferredIP,       // address
-		preferredHostname, // name
-		"alive",           // state
-		osName,            // os_name
-		workspaceId,       // workspace_id
-		now,               // updated_at
-		purpose,           // purpose
-	).Scan(&hostId)
-
+		ON CONFLICT (workspace_id, address) DO UPDATE SET 
+			mac = excluded.mac, 
+			name = excluded.name, 
+			state = excluded.state, 
+			os_name = excluded.os_name, 
+			purpose = excluded.purpose, 
+			created_at = excluded.created_at, 
+			updated_at = excluded.updated_at
+		RETURNING
+			id,
+			workspace_id, 
+			address, 
+			mac, 
+			name, 
+			state, 
+			os_name, 
+			purpose,
+			created_at, 
+			updated_at
+		`,
+		msfHost.WorkspaceId,
+		msfHost.Address.String(), // need to call String(), otherwise IPv4 addresses are inserted as IPv6
+		msfHost.MAC,
+		msfHost.Name,
+		msfHost.State,
+		msfHost.OSName,
+		msfHost.Purpose,
+		msfHost.CreatedAt,
+		msfHost.UpdatedAt,
+	).Scan(
+		&msfHost.Id,
+		&msfHost.WorkspaceId,
+		&msfHost.Address,
+		&msfHost.MAC,
+		&msfHost.Name,
+		&msfHost.State,
+		&msfHost.OSName,
+		&msfHost.Purpose,
+		&msfHost.CreatedAt,
+		&msfHost.UpdatedAt,
+	)
 	if err != nil {
-		return 0, fmt.Errorf("inserting host %s: %w", host, err)
+		return 0, fmt.Errorf("upsert host %s: %w", nmapHost, err)
 	}
 
-	log.Debugf("Inserted/updated host %s.", host)
+	log.Debugf("Inserted/updated host %s.", nmapHost)
 
 	openPortCount := 0
-	for _, port := range host.Ports.Port {
-		err := InsertService(ctx, tx, hostId, port)
+	for _, port := range nmapHost.Ports.Port {
+		err := InsertService(ctx, tx, msfHost.Id, port)
 		if err != nil {
-			return 0, fmt.Errorf("inserting port %s/%d for host %s: %w", port.Protocol, port.Portid, host, err)
+			return 0, fmt.Errorf("insert port %s/%d for host %s: %w", port.Protocol, port.Portid, nmapHost, err)
 		}
 
 		openPortCount++
@@ -108,7 +212,7 @@ func InsertHost(ctx context.Context, conn *pgx.Conn, workspaceId int, host NmapH
 
 	err = tx.Commit(ctx)
 	if err != nil {
-		return 0, fmt.Errorf("commiting transaction: %w", err)
+		return 0, fmt.Errorf("commit transaction: %w", err)
 	}
 
 	return openPortCount, nil
@@ -151,7 +255,7 @@ func InsertService(ctx context.Context, tx pgx.Tx, hostId int, service NmapServi
 	log.Debugf("Inserted/updated service %s.", service)
 
 	if err != nil {
-		return fmt.Errorf("inserting service %s: %w", service, err)
+		return fmt.Errorf("insert service %s: %w", service, err)
 	}
 
 	return nil
